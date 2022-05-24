c928acb05653d37bb500bd7fb5528133b67246a2
Junaid Farooq <junaid@evercam.io>
2022-05-24 09:22:20 +0500

    Start saving hashes to Slack Hashes state.

--- lib/etherscan/services/blocknative.ex
+++ lib/etherscan/services/blocknative.ex
@@ -1,10 +1,13 @@
 defmodule Etherscan.Services.Blocknative do
-  alias Etherscan.API.HTTP
+  alias Etherscan.{API.HTTP, Queue.Worker}
 
+  @spec add_to_watch(list(String.t())) ::
+          list(:ok)
   def add_to_watch(hashes) do
     hashes
     |> Task.async_stream(&do_send_request/1, max_concurrency: 10)
     |> Enum.to_list()
+    |> IO.inspect()
   end
 
   defp do_send_request(hash) do
@@ -18,6 +21,8 @@ defmodule Etherscan.Services.Blocknative do
         "network" => "main"
       })
     )
+
+    Worker.add_to_slack_hashes({DateTime.utc_now(), hash})
   end
 
   defp blocknative_api(), do: Application.get_env(:etherscan, :blocknative_api)
